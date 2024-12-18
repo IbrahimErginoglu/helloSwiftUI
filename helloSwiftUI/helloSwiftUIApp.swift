f464d6a3139b8eb281a10e6eda95da42bb7475ca
ibrahimerginoglu <ibrahim.erginoglu@hisarschool.k12.tr>
2024-12-18 21:39:51 +0300

    Initial Commit

---
+++ helloSwiftUI/helloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloSwiftUIApp.swift
+//  helloSwiftUI
+//
+//  Created by ibrahim erginoglu on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct helloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
